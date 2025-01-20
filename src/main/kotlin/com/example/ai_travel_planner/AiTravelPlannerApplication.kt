eea2e576ab03234658c63d278ad59452c4336534
Tatyana <Tatyanaantonova1212@gmail.com>
2025-01-20 16:36:02 +0200

    Init project

---
+++ src/main/kotlin/com/example/ai_travel_planner/AiTravelPlannerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ai_travel_planner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiTravelPlannerApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiTravelPlannerApplication>(*args)
+}
